5a9eed757249b6e1a9caf17a4dc2d31f113da50d
Alireza Rafiei <mail@rafiei.net>
2018-11-10 15:58:57 -0800

    text_evaluator

--- src/lang.py
+++ src/lang.py
@@ -1,39 +1,54 @@
-def evaluate(text, my_number=3100000000):
-    tokens = [token.lower() for token in text.split(' ')]
-    command = tokens[0]
-    if command == "create":
-        return {
-            "command": command,
-            "number": my_number
-        }
-    elif command == "send":
-        amount = tokens[1]
-        target = tokens[2]
-        return {
-            "command": command,
-            "amount": amount,
-            "target": target
-        }
-    elif command == "get":
-        topic = tokens[1]
-        assert topic in {"balance", "address", "history"}
-        assert tokens[2] == "of"
-        target = tokens[3]
-        assert target == "me" or (
-            str(int(target)) == target and len(target) == 10
-        )
-        if target == "me":
-            target = my_number
-        response = {
-            "command": command,
-            "topic": topic,
-            "target": target,
-        }
-        if len(tokens[4:]) != 0:
-            if tokens[4] == "since":
-                duration = tokens[5]
-                unit = tokens[6]
-                assert unit in ["day", "min", "week"]
-                response["duration"] = duration
-                response["unit"] = unit
-        return response
+class Commands(object):
+    @staticmethod
+    def Create(number):
+        raise NotImplementedError
+
+    @staticmethod
+    def Send(amount, from_, to):
+        raise NotImplementedError
+
+    def _GetBalance():
+        raise NotImplementedError
+
+    def _GetHistory(with_=None):
+        if with_ is not None:
+            raise NotImplementedError
+        raise NotImplementedError
+
+    @staticmethod
+    def Get(topic, *args, **kwargs):
+        getattr(Commands, f"_Get{topic.capitalize()}")(*args, **kwargs)
+
+
+class TextEvaluator(object):
+    def __init__(self, my_number):
+        self._number = my_number
+
+    @property
+    def my_number(self):
+        return self._number
+
+    def read(self, text):
+        tokens = [token.lower() for token in text.split(' ')]
+        command = tokens[0]
+        if command == "create":
+            return Commands.Create(self.my_number)
+        elif command == "send":
+            return Commands.Send(
+                amount=tokens[1],
+                from_=self.my_number,
+                to=tokens[2]
+            )
+        elif command == "get":
+            topic = tokens[1]
+            if topic == "balance":
+                return Commands.Get(topic)
+            elif topic == "history":
+                return Commands.Get(
+                    topic,
+                    with_=(
+                        False
+                        if len(tokens[2:]) == 0 or (tokens[2] != "with") else
+                        tokens[3]
+                    )
+                )
