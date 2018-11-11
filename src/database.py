2f28e9ff75b654ced8fae81742d4e35247e02240
jhpenger <apengjh@gmail.com>
2018-11-11 05:35:16 -0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -3,27 +3,32 @@ import pyotp
 import pyqrcode
 import cv2
 from PIL import Image
-
-def send(destination):
-    pass
-
-def get_balance(acc):
-    pass
+from eos import Admin, Commands
 
 class BackEnd:
     def __init__(self, client = None):
         self.client = MongoClient()
         self.db = self.client.project_sms
+        self.db.user_info.drop()
         self.user_info = self.db.user_info
-
-
+        self.admin = Admin()._id
+        
+        #create admin
+        self.user_info.insert({"_id" : 0,
+                               "num" : 0000000000,
+                               "seed" : None,
+                               "wallet" : self.admin
+                               })
+                               
+        
+        
     def get_balance(self, **args):
         #return self.user_info.find_one({"_id" : id}).get('balance', 0.0) if id else self.user_info.find_one({"num" : num}).get('balance', 0.0)
-        return 'yo balance is ', None
-
+        return 'Balance: %f' %0.0, None
+    
     def get_history(self, **args):
         return None, None
-
+    
     def send(self, **args):
         '''
         s_balance = self.get_balance(num = sender_num, id = sender_id)
@@ -35,13 +40,15 @@ class BackEnd:
         self.user_info.update_one(reciever, { "$set" : {"balance" : r_balance + amt}})
         return True
         '''
-        return "sending %s from your wallet to %s" %(args.get('amt'), args.get('To')), None
-
+        return "\nsent %s from your wallet to %s" %(args.get('amt'), args.get('To')), None
+    
     def verify(self, num, auth):
         user = self.user_info.find_one({'num' : num})
         user.get('seed')
         return pyotp.TOTP(user.get('seed')).verify(auth)
+    
 
+    
     def create(self, **args):
         #generate & store CRS
         From = args.get('From')
@@ -50,20 +57,24 @@ class BackEnd:
         qr = pyqrcode.create(totp)
         qr_location = 'qr_codes/%s.png' %From
         qr.png(qr_location, scale=5)
+        
         self.user_info.insert({"num" : From,
                                "seed" : seed,
-                               "qr" : qr_location})
-        ret_string = "A new account has been created for %s.\nYour 2FA key is: %s" %(From, seed)
-        print (ret_string)
+                               "qr" : qr_location,
+                               "wallet" : Commands.Create(From)
+                               })
+        
+        ret_string = "\nA new account has been created for %s.\nYour 2FA key is: %s" %(From, seed)
         return ret_string, {'totp' : totp, 'seed' : seed, 'media' : qr_location}
-
-
-
+    
 '''
-    data = {"_id" : None,
-            "name" : None,
-            "num" : None,
-            "balance" : 0.0,
-            "pub_key" : None
-            }
+{'name': 'user3111',
+'keys': {
+    'Owner': {'Private key': '5J7xUTwbuidUFdiHikfbf5Gs7NqfEfedHg8vGEbrgn16mi9ckZs',
+              'Public key': 'EOS5s3xjXMr1LxvMBg8Jz8f5bToXW4MPdz9EhfuR7eNyAwM2JGmLv'},
+    'Active': {'Private key': '5JfRi1hqHjembibW9YfHu1ApNM5t3uSr1aH6oqid49Pm78nVCtv',
+               'Public key': 'EOS54JLSmt3SuwUxibtJPqJ9HAh2dtrm8rtgaWonijy2bxaiwQ4VP'}
+    },
+    'password': 'PW5HxxXHC4EnGwyuKyAyV4uofGyQWfmFcXXYRqDQZPnYZXHV1KpYi'}
 '''
+        
