4c8cc146c1747ba28d510d87870b74eeb4d1947b
jhpenger <apengjh@gmail.com>
2018-11-11 11:45:17 -0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -14,23 +14,24 @@ class BackEnd:
         self.db.user_info.delete_many({})
         self.user_info = self.db.user_info
         self.admin = Admin()
-        
+
         #create admin
         self.user_info.insert({"_id" : 0,
                                "num" : 0000000000,
                                "seed" : None,
                                "wallet" : self.admin._id
                                })
-                               
-        
-        
+
+
+
     def get_balance(self, **args):
         #return self.user_info.find_one({"_id" : id}).get('balance', 0.0) if id else self.user_info.find_one({"num" : num}).get('balance', 0.0)
         #return str('Balance: %f.4d' %(Commands.Get(topic = "balance", admin_name = self.admin.name, of = args.get('From')))), None
-        return 'Balance: %s' %Commands._GetBalance(admin_name = self.admin.name, of = args.get('From')), None
+        name = self.user_info.find_one({"num" : args.get('From')}).get('wallet').get('name')
+        return 'Balance: %s' %Commands._GetBalance(admin_name = self.admin.name, of = name), None
     def get_history(self, **args):
         return None, None
-    
+
     def send(self, **args):
         '''
         s_balance = self.get_balance(num = sender_num, id = sender_id)
@@ -42,21 +43,27 @@ class BackEnd:
         self.user_info.update_one(reciever, { "$set" : {"balance" : r_balance + amt}})
         return True
         '''
-        admin_name, amount, from_, to = self.admin.name, args.get('amt'), args.get('From'), args.get('To')
+        from_ = self.user_info.find_one({"num" : args.get('From')}).get('wallet').get('name')
+        to = self.user_info.find_one({"num" : args.get('To')}).get('wallet').get('name')
+        admin_name, amount = self.admin.name, "{:.04f}".format(float(args.get('amt')))
+        print(admin_name)
+        print(from_)
+
+        print(to)
         success = Commands.Send(admin_name, amount, from_, to)
-        
+
         if success:
-            return "\nsent %s from your wallet to %s" %(args.get('amt'), args.get('To')), None
+            return "\nsent %s from your wallet to %s \nRemaining Balance: %s" %(args.get('amt'), args.get('To'), self.get_balance(From = args.get('From'))), None
         else:
             return "transaction failed", None
-    
+
     def verify(self, num, auth):
         user = self.user_info.find_one({'num' : num})
         user.get('seed')
         return pyotp.TOTP(user.get('seed')).verify(auth)
-    
 
-    
+
+
     def create(self, **args):
         #generate & store CRS
         From = args.get('From')
@@ -65,11 +72,11 @@ class BackEnd:
         qr = pyqrcode.create(totp)
         qr_location = 'qr_codes/%s.png' %From
         qr.png(qr_location, scale=5)
-        self.user_info.update_one({"num" : From}, 
+        self.user_info.update_one({"num" : From},
                               {"$set": {"num" : From,
                                "seed" : seed,
                                "qr" : qr_location,
-                               "wallet" : Commands.Create(From)
+                               "wallet" : Commands.Create(admin = self.admin)
                                }}, upsert=True)
         '''
         self.user_info.insert({"num" : From,
@@ -78,12 +85,12 @@ class BackEnd:
                                "wallet" : Commands.Create(From)
                                })
                                '''
-        
+
         ret_string = "\nA new account has been created for %s.\nYour 2FA key is: %s" %(From, seed)
         #ret_string = seed
         print(ret_string, {'totp' : totp, 'seed' : seed, 'media' : qr_location})
         return ret_string, {'totp' : totp, 'seed' : seed, 'media' : qr_location}
-    
+
 '''
 {'name': 'user3111',
 'keys': {
@@ -94,4 +101,3 @@ class BackEnd:
     },
     'password': 'PW5HxxXHC4EnGwyuKyAyV4uofGyQWfmFcXXYRqDQZPnYZXHV1KpYi'}
 '''
-        
